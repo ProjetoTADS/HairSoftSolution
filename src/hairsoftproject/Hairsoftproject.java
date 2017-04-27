b707b4b1ffaa134510ec27ffd668d3893fb17685
Projeto Uninove <segundoprojeto.uninove@gmail.com>
2017-04-27 15:42:11 -0300

    PRIMEIRO COMMIT DO REPOSITÓRIO

---
+++ src/hairsoftproject/Hairsoftproject.java
@@ -0,0 +1,37 @@
+package hairsoftproject;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Eduardo
+ */
+public class Hairsoftproject extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+     Parent root = FXMLLoader.load(getClass().getResource("TelaInicial.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        primaryStage.setTitle("Software HairSoft");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        primaryStage.setResizable(false);
+    }
+
+  
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ src/hairsoftproject/TelaInicialController.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hairsoftproject;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author Eduardo
+ */
+public class TelaInicialController implements Initializable {
+
+    @FXML
+    private AnchorPane stack;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    void cadastraCliente(){
+        stack.getChildren().clear();
+        stack.getChildren().add(getNode("cadastrarNovo.fxml"));
+    }
+    
+    @FXML
+    void verGrafico(){
+    stack.getChildren().clear();
+    stack.getChildren().add(getNode("grafico.fxml"));
+    }
+    
+    @FXML
+    void agendaCliente(){
+    stack.getChildren().clear();
+    stack.getChildren().add(getNode("agendarCliente.fxml"));
+    }
+    
+    @FXML
+    void visualizaAgenda(){
+    stack.getChildren().clear();
+    stack.getChildren().add(getNode("visualizaAgenda.fxml"));
+    }
+    
+    @FXML
+    void geraPDF(){
+    stack.getChildren().clear();
+    stack.getChildren().add(getNode("geraPDF.fxml"));
+    }
+    
+    @FXML
+        void ajuda(){
+    stack.getChildren().clear();
+    stack.getChildren().add(getNode("ajuda.fxml"));
+    }
+    
+    @FXML
+    void home(){
+        stack.getChildren().clear();
+    }
+    
+    public Node getNode(String node){
+        Node no = null;
+        try {
+            no = FXMLLoader.load(getClass().getResource(node));
+        } catch (Exception e) {
+        }
+        return no;
+        
+    }
+    
+}
